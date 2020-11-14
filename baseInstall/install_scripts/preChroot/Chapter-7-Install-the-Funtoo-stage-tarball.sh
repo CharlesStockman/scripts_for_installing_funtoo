c274579131c0eacab9e797669aa2577963d727b0
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2020-11-14 12:54:35 -0500

    Update Chapter-7-Install-the-Funtoo-stage-tarball.sh

--- baseInstall/install_scripts/preChroot/Chapter-7-Install-the-Funtoo-stage-tarball.sh
+++ baseInstall/install_scripts/preChroot/Chapter-7-Install-the-Funtoo-stage-tarball.sh
@@ -5,5 +5,12 @@
 
 cd /mnt/funtoo
 wget https://build.funtoo.org/1.4-release-std/x86-64bit/generic_64/stage3-latest.tar.xz
-tar --numeric-owner -xpf /mnt/funtoo/stage3-latest.tar.xz
+
+#
+# --numeric owner Preserves the numeric values of the UID and GID instead of mapping them
+# --xattrs, --xattrs-include An extended attribute which allows certain programs such as ping to have enhanced privileges without having
+#                            to be fully suid root
+#
+# -p preserve regular permissions and ownership
+tar --numeric-owner --xattrs --xattrs-include="*" -xpf /mnt/funtoo/stage3-latest.tar.xz
 rm /mnt/funtoo/stage3-latest.tar*
