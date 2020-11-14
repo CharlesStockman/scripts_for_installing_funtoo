4add4e137f4160b19df5b1d06957f876500f1c07
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2020-11-14 12:44:38 -0500

    Update Chapter-6-Setting-the-Date.sh

--- baseInstall/install_scripts/preChroot/Chapter-6-Setting-the-Date.sh
+++ baseInstall/install_scripts/preChroot/Chapter-6-Setting-the-Date.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# To set the date manually use the following command: date 071620002011
-#   where 07 is July, 16 is 16th where 2000 is 20:00 UTC and 2011 is the year
+# Copy the time so the hardware clock to the hardware close so it persists across reboots
+hwclock --systohc
