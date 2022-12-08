c8519928f9b37343f7681e1baa9755e4c22c7ed8
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2022-12-08 01:26:03 -0500

    Update Chapter-12-Introducing-Portage.sh
    Removed no longer needed code

--- baseInstall/install_scripts/postChroot/Chapter-12-Introducing-Portage.sh
+++ baseInstall/install_scripts/postChroot/Chapter-12-Introducing-Portage.sh
@@ -5,11 +5,6 @@
 #
 ego sync
 
-# (07/29/2020) -- Stage 3 has bindist USE Flag set ( control enabling/disabling for proprietary 
-# and/or patented part of the binaries.  When you update the packages after an intial setup.
-# To avoid problems before first boot run the following code
-#
-
 # -u	update the package we specify and if the package is already emerged don't emerge again
 # -D	Deep Update ( Update packages and dependencies )
 # -N	update packgages that have changed (new) USE setting
