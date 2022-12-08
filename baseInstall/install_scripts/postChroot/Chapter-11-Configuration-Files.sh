d4ed5cffb632e650eec96cde7d1947f345117433
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2022-12-08 00:23:57 -0500

    Update Chapter-11-Configuration-Files.sh
    Removed make.conf since it is no longer needed

--- baseInstall/install_scripts/postChroot/Chapter-11-Configuration-Files.sh
+++ baseInstall/install_scripts/postChroot/Chapter-11-Configuration-Files.sh
@@ -9,20 +9,3 @@ cp $fstab /etc/fstab
 # -s -- Symbolic
 # -f -- force
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
-
-#
-# CFLAGS and CXXFLAGS -- CFLAGS is meant for C programs and CXXFLAGS is meant for C++ programs
-# LINGUAS --  used by some gettext-based build systems to control which localization files are built and installed.
-#
-# wifi and -vala was choosen so network manager could install
-# examples and doc was choosen to more information about the commands
-# 
-# 
-useFlags="wifi -vala"
-compilerOptions="-march=skylake -O2 -pipe"
-
-echo "CFLAGS=\"$compilerOptions\"" 		>> /etc/portage/make.conf
-echo "CXXFLAGS=\"$compilerOptions\""		>> /etc/portage/make.conf
-echo "USE=\"$useFlags\""			>> /etc/portage/make.conf
-echo "MAKEOPTS=\"-j8\"" 			>> /etc/portage/make.conf
-echo "LINGUAS=\"en_us\""			>> /etc/portage.make.conf
