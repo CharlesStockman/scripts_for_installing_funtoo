bb259237c55c87e67ef3999fdd34edb9fa16e48b
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2022-12-11 13:14:16 -0500

    Update Chapter-17-Profiles.sh

--- baseInstall/install_scripts/afterReboot/Chapter-17-Profiles.sh
+++ baseInstall/install_scripts/afterReboot/Chapter-17-Profiles.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+#
+# Set the following profiles
+#
+epro arch x86-64bit
 epro subarch intel64-skylake
 epro flavor desktop
 epro profile mix-in gfxcard-nvidia
+epro profile mix-in gnome
+
+#
+# Update the new system 
+#  
+# -u    update to the new version possible.  May not be the current due to masking issues
+# -D    Updates all the software in the system including dependencies
+# -N    install any software based on the new use flgs
+emerge -auDN @world
+
+# Upate the system
+etc-update
+boot-update
