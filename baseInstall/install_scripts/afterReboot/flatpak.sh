01ff9b3932bc5555584b81c6081bc9ecfeea50c4
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2022-12-12 22:18:24 -0500

    Create flatpak.sh

---
+++ baseInstall/install_scripts/afterReboot/flatpak.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+#
+# Enable kernel to generate namespaces for non-root users
+#
+echo 1 > /proc/sys/kernel/unprivileged_userns_clone
+# echo 'kernel.unprivileged_userns_clone=1' > /etc/sysctl.d/unprivileged_userns_clone.conf
+
+#
+# ensure the fuse module is loaded by default
+#
+echo "modules=\"fuse\"" >> /etc/conf.d/modules
+modprobe fuse
+
+# 
+# Install Flatpak
+#
+emerge -v sys-apps/flatpak
+
+#
+# Install Flatpak Software
+# FlatPaks do not need root, but we want to make the script easier to write
+#
+flatpak install flathub com.valvesoftware.Steam
+
