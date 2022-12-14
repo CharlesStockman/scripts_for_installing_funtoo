e7a95383bd84eb0086d78903585c0abd0d56a039
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2022-12-13 21:02:07 -0500

    Update Chapter-13-Install-a-kernel.sh

--- baseInstall/install_scripts/postChroot/Chapter-13-Install-a-kernel.sh
+++ baseInstall/install_scripts/postChroot/Chapter-13-Install-a-kernel.sh
@@ -7,11 +7,10 @@
 #
 # Command line options for emerge ( -a is ask and -v is verbose )
 echo "sys-kernel/debian-sources binary" >  /etc/portage/package.use
-emerge -v debian-sources
 
 # 
 # Install sys-kernel/linux-firmware -- Starting at Broxton (a Skylake-based micro-architecture) 
 # Intel CPUs require binary blobs for additional low-power idle states (DMC), graphics workload 
 # scheduling on the various graphics parallel engines (GuC), and offloading some media functions 
 # from the CPU to GPU (HuC).[1]
-emerge -v linux-firmware
+emerge -v sys-kernel/linux-firmware
