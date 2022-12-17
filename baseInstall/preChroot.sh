5ffc16f42f5feba2bf8b50e4755cd29c5a9b24f1
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2022-12-17 14:04:37 -0500

    Update preChroot.sh

--- baseInstall/preChroot.sh
+++ baseInstall/preChroot.sh
@@ -1,26 +1,5 @@
 #/bin/sh
 
-user="CharlesStockman"
-project="scripts_for_installing_funtoo"
-branch="master"
-section="baseInstall"
-
-path=$user/$project/$branch
-site="raw.githubusercontent.com"
-
-preCommitDir="baseInstall/install_scripts/preChroot"
-
-if [ ! -d scripts ]
-then 
-	mkdir scripts
-	cd scripts
-fi
-
-wget https://$site/$path/$preCommitDir/Chapter-5-Creating-File-Systems.sh
-wget https://$site/$path/$preCommitDir/Chapter-5a-Mounting-Filesystems.sh
-wget https://$site/$path/$preCommitDir/Chapter-6-Setting-the-Date.sh
-wget https://$site/$path/$preCommitDir/Chapter-7-Install-the-Funtoo-stage-tarball.sh
-
 sh ./Chapter-5-Creating-File-Systems.sh
 sh ./Chapter-5a-Mounting-Filesystems.sh
 sh ./Chapter-6-Setting-the-Date.sh
