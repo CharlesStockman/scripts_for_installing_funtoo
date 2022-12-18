6a68f2aa99c1a29593f9d1adcc0db755b40b2ccb
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2022-12-18 00:28:40 -0500

    Update install_gnome.sh

--- baseInstall/install_scripts/afterReboot/install_gnome.sh
+++ baseInstall/install_scripts/afterReboot/install_gnome.sh
@@ -1,3 +1,8 @@
 #! /bin/sh 
 
+#
+# Install the gnome environment since that is the recommended desktop for Funtoo
+# I am installing this current desktop environemnt, but will new ones later on.
+#
+emerge sys-fs/fuse
 emerge @gnome
