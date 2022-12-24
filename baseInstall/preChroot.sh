d24f72738750247be465788121a7cb0c92613643
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2022-12-23 19:10:04 -0500

    Update preChroot.sh

--- baseInstall/preChroot.sh
+++ baseInstall/preChroot.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-sh ./scripts_for_installing_funtoo/baseInstall/Chapter-5-Creating-File-Systems.sh
-sh ./scripts_for_installing_funtoo/baseInstall/Chapter-5a-Mounting-Filesystems.sh
-sh ./scripts_for_installing_funtoo/baseInstall/Chapter-6-Setting-the-Date.sh
-sh ./scripts_for_installing_funtoo/baseInstall/Chapter-7-Install-the-Funtoo-stage-tarball.sh
+sh ./install_scripts/baseInstall/Chapter-5-Creating-File-Systems.sh
+sh ./install_scripts/baseInstall/Chapter-5a-Mounting-Filesystems.sh
+sh ./install_scripts/baseInstall/Chapter-6-Setting-the-Date.sh
+sh ./install_scripts/baseInstall/Chapter-7-Install-the-Funtoo-stage-tarball.sh
