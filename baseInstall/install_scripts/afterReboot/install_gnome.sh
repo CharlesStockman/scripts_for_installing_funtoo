9b3736446b9f2ff5deb1cc54592b1be192d3ff69
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2022-12-24 00:57:49 -0500

    Update install_gnome.sh

--- baseInstall/install_scripts/afterReboot/install_gnome.sh
+++ baseInstall/install_scripts/afterReboot/install_gnome.sh
@@ -4,8 +4,8 @@
 # Install the gnome environment since that is the recommended desktop for Funtoo
 # I am installing this current desktop environemnt, but will new ones later on.
 #
-emerge sys-fs/fuse
-emerge @gnome
+# emerge sys-fs/fuse
+emerge gnome
 
 #
 # configure openrc to autostart dbus and elogind:
