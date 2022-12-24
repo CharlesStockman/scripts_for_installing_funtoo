9ab1c4a3428ec3da685316a2f5e62729e52581f2
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2022-12-23 19:51:11 -0500

    Update postChroot.sh

--- baseInstall/postChroot.sh
+++ baseInstall/postChroot.sh
@@ -1,10 +1,10 @@
 #/bin/sh
 
-sh ./Chapter-10-Download-Portage-Tree.sh 	; echoEndString
-sh ./Chapter-11-Configuration-Files.sh		; echoEndString
-sh ./Chapter-13-Install-a-kernel.sh 		; echoEndString
-sh ./Chapter-14-Bootloader.sh			; echoEndString
-sh ./Chapter-15-Networkmanager.sh		; echoEndString
-sh ./Chapter-16-Finishing-up.sh			; echoEndString
-sh ./Chapter-16-Configuring-Random.sh		; echoEndString
-sh ./Chapter-17-Profiles.sh                     ; echoEndString
+sh ./baseInstall/install_scripts/Chapter-10-Download-Portage-Tree.sh 	; echoEndString
+sh ./baseInstall/install_scripts/Chapter-11-Configuration-Files.sh		; echoEndString
+sh ./baseInstall/install_scripts/Chapter-13-Install-a-kernel.sh 		; echoEndString
+sh ./baseInstall/install_scripts/Chapter-14-Bootloader.sh			; echoEndString
+sh ./baseInstall/install_scripts/Chapter-15-Networkmanager.sh		; echoEndString
+sh ./baseInstall/install_scripts/Chapter-16-Finishing-up.sh			; echoEndString
+sh ./baseInstall/install_scripts/Chapter-16-Configuring-Random.sh		; echoEndString
+sh ./baseInstall/install_scripts/Chapter-17-Profiles.sh                     ; echoEndString
