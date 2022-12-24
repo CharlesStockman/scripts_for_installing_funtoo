4e931a2284eb1fa900b96eed54742fb2bf6321cd
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2022-12-24 09:36:12 -0500

    Create create_xintitrc.sh

---
+++ baseInstall/install_scripts/afterReboot/create_xintitrc.sh
@@ -0,0 +1,28 @@
+#
+# Create and copy the .xinitrc file to the correct user
+#
+
+#
+# Verify arguments
+# 
+if [ -z $1 ]
+then
+  echo "No user has been specified.  A user is needed to know where the script should be copied to."
+else 
+  $user = $1
+fi
+
+#
+# Create the xinitrc file
+#
+cat > xinitrc << "END"
+# Fix Missing Applications in Gnome
+export XDG_MENU_PREFIX=gnome-
+
+# Properly Launch the Desired X Session
+exec gnome-session
+END
+
+cp xinitrc /home/$1/.xinitrc
+chown $1:$1 /home/$1/.xinitrc
+chmod 644 /home/$1/.xinitrc
