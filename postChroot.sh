c1953221a091538d383b4f68954509bc4eb43263
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2022-12-25 00:00:25 -0500

    Update postChroot.sh

--- postChroot.sh
+++ postChroot.sh
@@ -1,10 +1,10 @@
 #/bin/sh
 
-sh ./install_scripts/postChroot/Chapter-10-Download-Portage-Tree.sh 	; echoEndString
-sh ./install_scripts/postChroot/Chapter-11-Configuration-Files.sh		  ; echoEndString
-sh ./install_scripts/postChroot/Chapter-13-Install-a-kernel.sh 		    ; echoEndString
-sh ./install_scripts/postChroot/Chapter-14-Bootloader.sh			        ; echoEndString
-sh ./install_scripts/postChroot/Chapter-15-Networkmanager.sh		      ; echoEndString
-sh ./install_scripts/postChroot/Chapter-16-Finishing-up.sh			      ; echoEndString
-sh ./install_scripts/postChroot/Chapter-16-Configuring-Random.sh		  ; echoEndString
-sh ./install_scripts/postChroot/Chapter-17-Profiles.sh                ; echoEndString
+sh ./install_scripts/postChroot/Chapter-10-Download-Portage-Tree.sh 	
+sh ./install_scripts/postChroot/Chapter-11-Configuration-Files.sh		  
+sh ./install_scripts/postChroot/Chapter-13-Install-a-kernel.sh 		   
+sh ./install_scripts/postChroot/Chapter-14-Bootloader.sh			        
+sh ./install_scripts/postChroot/Chapter-15-Networkmanager.sh		      
+sh ./install_scripts/postChroot/Chapter-16-Finishing-up.sh			      
+sh ./install_scripts/postChroot/Chapter-16-Configuring-Random.sh		  
+sh ./install_scripts/postChroot/Chapter-17-Profiles.sh                
