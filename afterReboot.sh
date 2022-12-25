3ffb96d2334e9ddfa2fce6edda4d59d20718e1d0
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2022-12-24 23:57:09 -0500

    Create afterReboot.sh

---
+++ afterReboot.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+
+sh ./install_scripts/afterReboot/Chapter-17-profiles.sh 
+sh ./install_scripts/afterReboot/install_gnome.sh
+sh ./install_scripts/afterReboot/nvidia_install.sh
+sh ./install_scripts/afterReboot/create_xinitrc.sh
+sh ./install_scripts/afterReboot/flatpak.sh
