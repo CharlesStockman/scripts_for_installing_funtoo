1c3cb85e28f13102165c8761e8f85e3b952a9d80
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2022-12-27 02:26:34 -0500

    Update afterReboot.sh

--- afterReboot.sh
+++ afterReboot.sh
@@ -5,3 +5,4 @@ sh ./install_scripts/afterReboot/install_gnome.sh
 sh ./install_scripts/afterReboot/nvidia_install.sh
 sh ./install_scripts/afterReboot/create_xinitrc.sh
 sh ./install_scripts/afterReboot/flatpak.sh
+sh ./install_scripts/afterReboot/setupForLinuxConfiguration.sh
